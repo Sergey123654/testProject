d238c3affe10037671d256ed73167e55a5728266
sergey_schegolihin <sergey.schegolihin@mail.ru>
2021-09-18 16:53:19 +0300

    Тестовый проект

---
+++ CommandService/Controllers/PlatformsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandService.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class PlatformsController:ControllerBase
+    {
+        
+    }
+}
